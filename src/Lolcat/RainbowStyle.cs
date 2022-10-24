d4b5fa987dc76de5b456f85692d8c6c8031e382c
Simon McKenna <sshnug.si+github@gmail.com>
2022-10-25 00:33:14 +1030

    dotnet format

--- src/Lolcat/RainbowStyle.cs
+++ src/Lolcat/RainbowStyle.cs
@@ -9,5 +9,5 @@ public record RainbowStyle(
 	public EscapeSequence EscapeSequence { get; set; } = EscapeSequence;
 	public double Spread { get; set; } = Spread.ThrowIfOutOfRange(1, 10);
 	public double Frequency { get; set; } = Frequency.ThrowIfOutOfRange(0.001, 1);
-	public int? Seed { get; set;  } = Seed;
+	public int? Seed { get; set; } = Seed;
 }
