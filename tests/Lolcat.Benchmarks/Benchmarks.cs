80b6256ade40b46b2a20d6170926f47298bc38c7
Simon McKenna <sshnug.si+github@gmail.com>
2022-10-25 23:54:32 +1030

    Add some benchmarks

--- tests/Lolcat.Benchmarks/Benchmarks.cs
+++ tests/Lolcat.Benchmarks/Benchmarks.cs
@@ -3,15 +3,39 @@
 [Config(typeof(BenchmarkConfig))]
 public class Benchmarks
 {
-    [Benchmark]
-    public void Scenario1()
-    {
-        // Implement your benchmark here
-    }
+    private readonly Rainbow _ansiRainbow = new (new RainbowStyle());
+    private readonly Rainbow _spectreRainbow = new (new RainbowStyle(EscapeSequence.Spectre));
 
     [Benchmark]
-    public void Scenario2()
+    [ArgumentsSource(nameof(TextToConvert))]
+    // ReSharper disable once InconsistentNaming
+    public string ConvertToAnsi(string Text) => _ansiRainbow.Convert(Text);
+
+    [Benchmark]
+    [ArgumentsSource(nameof(TextToConvert))]
+    // ReSharper disable once InconsistentNaming
+    public string ConvertToSpectre(string Text) => _spectreRainbow.Convert(Text);
+
+    public IEnumerable<object> TextToConvert()
     {
-        // Implement your benchmark here
+        yield return AsciiSet(1);
+        yield return AsciiSet(10);
+        yield return AsciiSet(100);
+    }
+
+    private static string AsciiSet(int count)
+    {
+        var set = new StringBuilder();
+
+        var chars = CollectionsMarshal.AsSpan(
+        Enumerable.Range(32, 123).Select(i => (char)i).ToList());
+
+        for (var i = 0; i < count; i++)
+        {
+            set.Append(chars);
+            set.AppendLine();
+        }
+
+        return set.ToString();
     }
 }
--- tests/Lolcat.Benchmarks/GlobalUsings.cs
+++ tests/Lolcat.Benchmarks/GlobalUsings.cs
@@ -1,3 +1,5 @@
 global using BenchmarkDotNet.Attributes;
 global using BenchmarkDotNet.Running;
 global using Lolcat.Benchmarks;
+global using System.Runtime.InteropServices;
+global using System.Text;
